5e4bd2625be605e3260e2790c57f4ebed864c687
AndreSDS <andre_smiths@outlook.com>
2020-06-21 17:19:49 -0300

    testando styled components

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,5 +1,54 @@
-import React from "react";
+import React, { Component } from "react";
+//import deafultBcg from "../images/room-1.jpg";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
+import { RoomContext } from "../context";
 
-export default function SingleRomm() {
-  return <div>Hello from page singleroom</div>;
+export default class SingleRomm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      slug: this.props.match.params.slug
+    };
+  }
+
+  static contextType = RoomContext;
+
+  render() {
+    const { getRoom } = this.context;
+    const room = getRoom(this.state.slug);
+    if (!room) {
+      return (
+        <div className="error">
+          <h3>no such room could de found...</h3>
+          <Link to="/rooms" className="btn-primary">
+            back to rooms
+          </Link>
+        </div>
+      );
+    }
+
+    const {
+      name,
+      description,
+      capacity,
+      size,
+      price,
+      extras,
+      breackfast,
+      pets,
+      images
+    } = room;
+
+    return (
+      <Hero hero="roomsHero">
+        <Banner title={`${name} room`}>
+          <Link to="/rooms" className="btn-primary">
+            back to rooms
+          </Link>
+        </Banner>
+      </Hero>
+    );
+  }
 }
