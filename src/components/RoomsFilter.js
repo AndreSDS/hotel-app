1195b2eb0a654cffbf53defa755274fa36021d52
andresds <andre_smiths@outlook.com>
2020-06-25 13:10:01 -0300

    filtrando por type e capacity

--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -1,11 +1,72 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { RoomContext } from "../context";
+import Title from "../components/Title";
 
-function RoomsFilter() {
+const getUnique = (items, value) => {
+  return [...new Set(items.map((item) => item[value]))];
+};
+
+function RoomsFilter({ rooms }) {
+  const context = useContext(RoomContext);
+  const {
+    handleChange,
+    type,
+    capacity,
+    price,
+    minPrice,
+    maxPrice,
+    minSize,
+    maxSize,
+    breakfast,
+    pets,
+  } = context;
+
+  let types = getUnique(rooms, "type");
+  types = ["all", ...types];
+  types = types.map((item, index) => {
     return (
-        <div>
-            Filter
+      <option value={item} key={index}>
+        {item}
+      </option>
+    );
+  });
+
+  let people = getUnique(rooms, 'capacity');
+  people = people.map((item,index)=>{
+  return <option key={index} value={item}>{item}</option>
+  });
+
+  return (
+    <section className="filter-container">
+      <Title title="search rooms" />
+      <form className="filter-form">
+        <div className="form-group">
+          <label htmlFor="type">room type</label>
+          <select
+            name="type"
+            id="type"
+            value={type}
+            className="form-control"
+            onChange={handleChange}
+          >
+            {types}
+          </select>
         </div>
-    )
+        <div className="form-group">
+          <label htmlFor="capacity">guest room</label>
+          <select
+            name="capacity"
+            id="capacity"
+            value={capacity}
+            className="form-control"
+            onChange={handleChange}
+          >
+            {people}
+          </select>
+        </div>
+      </form>
+    </section>
+  );
 }
 
 export default RoomsFilter;
--- src/components/RoomsList.js
+++ src/components/RoomsList.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import Room from './Room';
 
-function RoomsList() {
+function RoomsList({rooms}) {
+    if(rooms.length === 0) {
+        return (
+            <div className="empty-search">
+                <h3>unfortunately no rooms matched your search parameters</h3>
+            </div>
+        );
+    }
     return (
-        <div>
-            List
-        </div>
+        <section className="roomslist">
+            <div className="roomslist-center">
+                {
+                    rooms.map(item => {
+                        return <Room key={rooms.id} room={item} />;
+                    })
+                }
+            </div>
+        </section>
     )
 }
 
