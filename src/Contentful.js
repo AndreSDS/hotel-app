3d720411760a6439f831cc2482951ab0dcb77fe7
andresds <andre_smiths@outlook.com>
2020-06-25 16:08:30 -0300

    criando variaveis de ambiente

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,6 @@
 import { createClient } from "contentful";
 
 export default createClient({
-  space: "akmypftpo4j7",
-  accessToken: "82RFf8C-yqvX6LZvcAW9sekmQKnkLp9W6KdxhgzGLWo",
+  space: process.env.REACT_APP_API_SPACE,
+  accessToken: process.env.REACT_APP_ACCESS_TOKEN,
 });
